4e068bf170b80ae4cb1d029f81c091dc0c42121c
Edison Rosales <edisonrosales94@gmail.com>
2020-12-13 18:23:02 -0500

    [#173933037] validacion cedula

--- src/app/pages/formproveedor/formproveedor.component.ts
+++ src/app/pages/formproveedor/formproveedor.component.ts
@@ -9,8 +9,6 @@ import {AngularFireStorage} from '@angular/fire/storage';
 import {FirebaseServicioService} from '../../services/firebase-servicio.service';
 import { Observable } from 'rxjs';
 import cars from '../../../assets/json/cars.json';
-import { getLocaleDateFormat } from '@angular/common';
-
 
 @Component({
   selector: 'app-formproveedor',
@@ -68,7 +66,7 @@ export class FormproveedorComponent implements OnInit {
     private proveedorServicio: ProveedorService,
     private vehiculoServicio:VehiculoService,
     private firebaseServicio:FirebaseServicioService,
-    private storage: AngularFireStorage
+    private storage: AngularFireStorage,
    ) { }
 
    ngOnInit(): void {
@@ -111,7 +109,7 @@ export class FormproveedorComponent implements OnInit {
     this.vehiculo=this.secondFormGroup.value;
     console.log(this.proveedor);
     console.log(this.vehiculo);
-    /*this.firebaseServicio.SignUp(this.proveedor.emailDriver,'adwri545')
+   this.firebaseServicio.SignUp(this.proveedor.emailDriver,'adwri545')
     .then((value)=>{
       console.log("valor"+value);
       this.firebaseServicio.sendPasswordResetEmail(this.proveedor.emailDriver)
@@ -137,7 +135,7 @@ export class FormproveedorComponent implements OnInit {
 
     .catch((e)=>{
       alert(e);  
-    })*/
+    })
   }
   else{
     alert("Registro incompleto");
@@ -200,6 +198,70 @@ export class FormproveedorComponent implements OnInit {
     console.log("lalalala");
     var cedulaProveedor = (<HTMLInputElement>document.getElementById("cedula")).value;
    console.log("cedula prov",cedulaProveedor);
+   //var cedula:boolean = this.validate(cedulaProveedor);
+    //console.log("validez documento",cedula);
+  }
+  if(cedula){
+    this.firstFormGroup.controls['ciDriver'].setErrors({'incorrect':false});
+  }
+
+  validate(){
+    var cedula = (<HTMLInputElement>document.getElementById("cedula")).value;
+    if (cedula.length === 10) {
+      const digitoRegion = cedula.substring(0, 2);
+      if (digitoRegion >= String(0) && digitoRegion <= String(24)) {
+        const ultimoDigito = Number(cedula.substring(9, 10));
+        const pares = Number(cedula.substring(1, 2)) + Number(cedula.substring(3, 4)) + Number(cedula.substring(5, 6)) + Number(cedula.substring(7, 8));
+
+        let numeroUno: any = cedula.substring(0, 1);
+        numeroUno = (numeroUno * 2);
+        if (numeroUno > 9) {
+          numeroUno = (numeroUno - 9);
+        }
+
+        let numeroTres: any = cedula.substring(2, 3);
+        numeroTres = (numeroTres * 2);
+        if (numeroTres > 9) {
+          numeroTres = (numeroTres - 9);
+        }
+
+        let numeroCinco: any = cedula.substring(4, 5);
+        numeroCinco = (numeroCinco * 2);
+        if (numeroCinco > 9) {
+          numeroCinco = (numeroCinco - 9);
+        }
+
+        let numeroSiete: any = cedula.substring(6, 7);
+        numeroSiete = (numeroSiete * 2);
+        if (numeroSiete > 9) {
+          numeroSiete = (numeroSiete - 9);
+        }
+
+        let numeroNueve: any = cedula.substring(8, 9);
+        numeroNueve = (numeroNueve * 2);
+        if (numeroNueve > 9) {
+          numeroNueve = (numeroNueve - 9);
+        }
+        const impares = numeroUno + numeroTres + numeroCinco + numeroSiete + numeroNueve;
+        const sumaTotal = (pares + impares);
+        const primerDigitoSuma = String(sumaTotal).substring(0, 1);
+        const decena = (Number(primerDigitoSuma) + 1) * 10;
+        let digitoValidador = decena - sumaTotal;
+        if (digitoValidador === 10) {
+          digitoValidador = 0;
+        }
+        if (digitoValidador === ultimoDigito) {
+          this.firstFormGroup.controls['ciDriver'].setErrors({'incorrect':false});
+
+        } else {
+          this.firstFormGroup.controls['ciDriver'].setErrors({'incorrect':true});
+        }
+      } else {
+        this.firstFormGroup.controls['ciDriver'].setErrors({'incorrect':true});
+      }
+    } else {
+      this.firstFormGroup.controls['ciDriver'].setErrors({'incorrect':true});
+    }
   }
 
 }

d921a090c2bfd59f1f0325d6876e8fcaa7a55646
Edison Rosales <edisonrosales94@gmail.com>
2020-12-14 23:33:56 -0500

    [#173927417] implementa servicio para endpoints para empresas

---
+++ src/app/services/admin.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+  API_URI='https://ctvehicular.pythonanywhere.com/api';
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  getEmpresas(){
+    return this.http.get(`${this.API_URI}/company/`);
+  }
+
+  createEmpresa(empresa:any){
+    return this.http.post(`${this.API_URI}/company/`,empresa);
+  }
+}
